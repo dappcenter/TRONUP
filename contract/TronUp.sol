f7c5815c27ff24e3f30a4b6c1b02dd373403ceef
tronup <51105662+tronup@users.noreply.github.com>
2019-06-04 15:34:48 +0800

    Create TronUp.sol

---
+++ contract/TronUp.sol
@@ -0,0 +1,470 @@
+pragma solidity =0.4.25;
+
+import "./SafeMath.sol";
+import "./Ownable.sol";
+
+/**
+ * @title ITRC20 interface
+ */
+interface ITRC20 {
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function approve(address spender, uint256 value) external returns (bool);
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function allowance(address account, address spender) external view returns (uint256);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed account, address indexed spender, uint256 value);
+}
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * https://eips.ethereum.org/EIPS/eip-20
+ * Originally based on code by FirstBlood:
+ * https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ *
+ * This implementation emits additional Approval events, allowing applications to reconstruct the allowance status for
+ * all accounts just by listening to said events. Note that this isn't required by the specification, and other
+ * compliant implementations may not do it.
+ */
+contract TRC20 is ITRC20, Ownable {
+    using SafeMath for uint256;
+
+    mapping (address => uint256) internal _balances;
+
+    mapping (address => mapping (address => uint256)) internal _allowed;
+
+    uint256 internal _totalSupply;
+    uint256 internal _totalMint;
+    uint256 internal _totalBurnt;
+    uint256 internal _totalBurntCount;
+    
+    /**
+     * @dev Total number of tokens in existence
+     */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+    
+    function totalMint() public view returns (uint256) {
+        return _totalMint;
+    }
+    
+    function totalBurnt() public view returns (uint256) {
+        return _totalBurnt;
+    }
+    
+    function totalBurntCount() public view returns (uint256) {
+        return _totalBurntCount;
+    }
+
+    /**
+     * @dev Gets the balance of the specified address.
+     * @param account The address to query the balance of.
+     * @return A uint256 representing the amount owned by the passed address.
+     */
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+     * @dev Function to check the amount of tokens that an account allowed to a spender.
+     * @param account address The address which owns the funds.
+     * @param spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(address account, address spender) public view returns (uint256) {
+        return _allowed[account][spender];
+    }
+
+    /**
+     * @dev Transfer token to a specified address
+     * @param to The address to transfer to.
+     * @param value The amount to be transferred.
+     */
+    function transfer(address to, uint256 value) public returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param spender The address which will spend the funds.
+     * @param value The amount of tokens to be spent.
+     */
+    function approve(address spender, uint256 value) public returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another.
+     * Note that while this function emits an Approval event, this is not required as per the specification,
+     * and other compliant implementations may not emit the event.
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param value uint256 the amount of tokens to be transferred
+     */
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        _transfer(from, to, value);
+        _approve(from, msg.sender, _allowed[from][msg.sender].sub(value));
+        return true;
+    }
+
+    /**
+     * @dev Increase the amount of tokens that an account allowed to a spender.
+     * approve should be called when _allowed[msg.sender][spender] == 0. To increment
+     * allowed value is better to use this function to avoid 2 calls (and wait until
+     * the first transaction is mined)
+     * From MonolithDAO Token.sol
+     * Emits an Approval event.
+     * @param spender The address which will spend the funds.
+     * @param addedValue The amount of tokens to increase the allowance by.
+     */
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowed[msg.sender][spender].add(addedValue));
+        return true;
+    }
+
+    /**
+     * @dev Decrease the amount of tokens that an account allowed to a spender.
+     * approve should be called when _allowed[msg.sender][spender] == 0. To decrement
+     * allowed value is better to use this function to avoid 2 calls (and wait until
+     * the first transaction is mined)
+     * From MonolithDAO Token.sol
+     * Emits an Approval event.
+     * @param spender The address which will spend the funds.
+     * @param subtractedValue The amount of tokens to decrease the allowance by.
+     */
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowed[msg.sender][spender].sub(subtractedValue));
+        return true;
+    }
+
+    /**
+     * @dev Transfer token for a specified addresses
+     * @param from The address to transfer from.
+     * @param to The address to transfer to.
+     * @param value The amount to be transferred.
+     */
+    function _transfer(address from, address to, uint256 value) internal {
+        require(to != address(0));
+
+        _balances[from] = _balances[from].sub(value);
+        _balances[to] = _balances[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+    /**
+     * @dev Internal function that mints an amount of the token and assigns it to
+     * an account. This encapsulates the modification of balances such that the
+     * proper events are emitted.
+     * @param account The account that will receive the created tokens.
+     * @param value The amount that will be created.
+     */
+    function _mint(address account, uint256 value) internal {
+        require(account != address(0));
+
+        _totalMint = _totalMint.add(value);
+        _balances[address(this)] = _balances[address(this)].sub(value);
+        _balances[account] = _balances[account].add(value);
+        emit Transfer(address(this), account, value);
+    }
+
+    /**
+     * @dev Internal function that burns an amount of the token of a given
+     * account.
+     * @param account The account whose tokens will be burnt.
+     * @param value The amount that will be burnt.
+     */
+    function _burn(address account, uint256 value) internal {
+        require(account != address(0));
+
+        _totalSupply = _totalSupply.sub(value);
+        _totalBurnt = _totalBurnt.add(value);
+        _totalBurntCount = _totalBurntCount.add(1);
+        _balances[account] = _balances[account].sub(value);
+        emit Transfer(account, address(0), value);
+    }
+
+    /**
+     * @dev Approve an address to spend another addresses' tokens.
+     * @param account The address that owns the tokens.
+     * @param spender The address that will spend the tokens.
+     * @param value The number of tokens that can be spent.
+     */
+    function _approve(address account, address spender, uint256 value) internal {
+        require(spender != address(0));
+        require(account != address(0));
+
+        _allowed[account][spender] = value;
+        emit Approval(account, spender, value);
+    }
+    
+}
+
+contract TronUp is TRC20 {
+    string public name = "TRON UP";
+    string public symbol = "UP";
+    uint8  public decimals = 6;
+    
+    uint256 internal _totalFrozen;
+    uint256 private upPrice = 1000000;
+    
+    event Frozen(address account, uint256 value);
+    event Unfrozen(address account, uint256 value);
+    event UpPriceUpdate(uint256 upPrice, uint256 time);
+
+    mapping (address => uint256) private _frozen;
+    mapping (address => uint256) private _unfrozen_2;
+    mapping (address => uint256) private _last_unfrozen_time_2;
+    mapping (address => uint256) private _unfrozen_24;
+    mapping (address => uint256) private _last_unfrozen_time_24;
+    
+    modifier onlyMinter() {
+        require(approvedMinters[msg.sender] == true);
+        _;
+    }
+    
+    mapping(address => bool) private approvedMinters;
+    
+     /**
+     * @dev constructor function.
+     */
+    constructor () public {
+        _totalSupply = 1000000000000000; 
+        _balances[address(this)] = _totalSupply;
+    }
+    
+    function addMinter(address _minter) public onlyOwner {
+        approvedMinters[_minter] = true;
+    }
+    
+    function removeMinter(address _minter) public onlyOwner {
+        approvedMinters[_minter] = false;
+    }
+    
+    function updateUpPrice(uint256 _upPrice) public onlyOwner {
+        upPrice = _upPrice;
+        emit UpPriceUpdate(upPrice, now);
+    }
+    
+    function mintScale() public view returns (uint256) {
+        uint256 scale = uint256(3).mul(uint256(10 ** 10)).div(upPrice);
+        return scale;
+    }
+    
+    function getUpPrice() public view returns(uint256) {
+        return upPrice;
+    }
+    
+    /**
+     * @dev increase token totalSupply.
+     * @param value uint256 The amount of token to be increased.
+     */
+    function mintToken(uint256 value) public onlyOwner {
+        _totalSupply = _totalSupply.add(value);
+        _balances[address(this)] = _balances[address(this)].add(value);
+    }
+    
+    /**
+    * @dev Gets the 12 hours thaws token of the specified address.
+    * @return An uint256 representing the amount thaws by the passed address.
+    */
+    function totalFrozen() public view returns (uint256) {
+        return _totalFrozen;
+    }
+    
+    /**
+    * @dev Gets the 12 hours thaws token of the specified address.
+    * @param account The address to query the frozen of.
+    * @return An uint256 representing the amount thaws by the passed address.
+    */
+    function frozenOf(address account) public view returns (uint256) {
+        return _frozen[account];
+    }
+    
+    /**
+    * @dev Gets the 12 hours thaws token of the specified address.
+    * @param account The address to query the frozen of.
+    * @return An uint256 representing the amount thaws by the passed address.
+    */
+    function unfrozen2HoursOf(address account) public view returns (uint256) {
+        return _unfrozen_2[account];
+    }
+
+    /**
+    * @dev Gets the 24 hours thaws token of the specified address.
+    * @param account The address to query the frozen of.
+    * @return An uint256 representing the amount thaws by the passed address.
+    */
+    function unfrozen24HoursOf(address account) public view returns (uint256) {
+        return _unfrozen_24[account];
+    }
+
+    /**
+    * @dev Gets the last 12 hours thaw time of the specified address.
+    * @param account The address to query the last 12 hours thaw time of.
+    * @return An uint256 representing the last 12 hours thaw time by the passed address.
+    */
+    function lastUnfrozenTime2Hours(address account) public view returns (uint256) {
+        return _last_unfrozen_time_2[account];
+    }
+
+    /**
+    * @dev Gets the last 24 hours thaw time of the specified address.
+    * @param account The address to query the last 24 hours thaw time of.
+    * @return An uint256 representing the last 24 hours thaw time by the passed address.
+    */
+    function lastUnfrozenTime24Hours(address account) public view returns (uint256) {
+        return _last_unfrozen_time_24[account];
+    }
+    
+    function transferByMinter(address from, address to, uint256 value) public onlyMinter returns (bool) {
+        _transfer(from, to, value);
+        return true;
+    }
+    
+    /**
+    * @dev Freeze token.
+    * @param value The amount to be frozen.
+    */
+    function freeze(uint256 value) public returns (bool) {
+        require( value >= 10000000, "Don't equal 0x0000000000000000000000000000000000000000");
+        require(_balances[msg.sender] >= value, "balance of msg.sender more than value.");
+        
+        _totalFrozen = _totalFrozen.add(value);
+        _balances[msg.sender] = _balances[msg.sender].sub(value);
+        _frozen[msg.sender] = _frozen[msg.sender].add(value);
+        
+        emit Frozen(msg.sender, value);
+        return true;
+    }
+
+    /**
+    * @dev Thaw 12 hours token.
+    */
+    function unfrozen2Hours() public returns (bool) {
+        require( _frozen[msg.sender] >= 0, "Frozen UPs must be more than 0.");
+
+        _last_unfrozen_time_2[msg.sender] = now + 2 hours;
+
+        uint256 value = _frozen[msg.sender];
+        _frozen[msg.sender] = 0;
+
+        _totalFrozen = _totalFrozen.sub(value);
+        //销毁30%
+        uint256 _burned = value.mul(30).div(100);
+        _burn(msg.sender, _burned);
+
+        _unfrozen_2[msg.sender] = _unfrozen_2[msg.sender].add(value.sub(_burned));
+        emit Unfrozen(msg.sender, value);
+
+        return true;
+    }
+
+    /**
+    * @dev Thaw 24 hours token.
+    */
+    function unfrozen24Hours() public returns (bool) {
+        require( _frozen[msg.sender] >= 0, "Frozen UPs must be more than 0.");
+
+        _last_unfrozen_time_24[msg.sender] = now + 24 hours;
+        
+        uint256 value = _frozen[msg.sender];
+        _frozen[msg.sender] = 0;
+        _totalFrozen = _totalFrozen.sub(value);
+        _unfrozen_24[msg.sender] = _unfrozen_24[msg.sender].add(value);
+
+        emit Unfrozen(msg.sender, value);
+        return true;
+    }
+
+    /**
+    * @dev Withdraw all 12 hours thawed tokens. Last thaw time must be out of 12 hours.
+    */
+    function withdraw2Hours() public returns (bool) {
+        require( _unfrozen_2[msg.sender] > 0, "Unfrozen of Ups should not be empty." );
+        require( now > _last_unfrozen_time_2[msg.sender], "Withdraw 12 hours after Unfrozen.");
+
+        uint256 value = _unfrozen_2[msg.sender];
+        _unfrozen_2[msg.sender] = 0;
+        _last_unfrozen_time_2[msg.sender] = 0;
+        _balances[msg.sender] = _balances[msg.sender].add(value);
+        emit Transfer(address(this), msg.sender, value);
+        
+        return true;
+    }
+
+    /**
+    * @dev Withdraw all 24 hours thawed tokens. Last thaw time must be out of 24 hours.
+    */
+    function withdraw24Hours() public returns (bool) {
+        require( _unfrozen_24[msg.sender] > 0, "Unfrozen of Ups should not be empty." );
+        require( now > _last_unfrozen_time_24[msg.sender], "Withdraw 24 hours after Unfrozen.");
+
+        uint256 value = _unfrozen_24[msg.sender];
+        _unfrozen_24[msg.sender] = 0;
+        _last_unfrozen_time_24[msg.sender] = 0;
+        _balances[msg.sender] = _balances[msg.sender].add(value);
+        emit Transfer(address(this), msg.sender, value);
+        
+        return true;
+    }
+
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param value The amount of token to be burned.
+     */
+    function burn(uint256 value) public {
+        _burn(msg.sender, value);
+    }
+    
+    /**
+     * @dev Internal function that burns an amount of the token of a given
+     * account, deducting from the sender's allowance for said account. Uses the
+     * internal burn function.
+     * Emits an Approval event (reflecting the reduced allowance).
+     * @param from The account whose tokens will be burnt.
+     * @param value The amount that will be burnt.
+     */
+    function burnFrom(address from, uint256 value) internal {
+        _burn(from, value);
+        _approve(from, msg.sender, _allowed[from][msg.sender].sub(value));
+    }
+    
+    /**
+     * @dev Function to mint tokens
+     * @param to The address that will receive the minted tokens.
+     * @param value The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mintByMinter(address to, uint256 value) public onlyMinter returns (bool) {
+        _mint(to, value);
+        return true;
+    }
+
+    /**
+     * @dev Burn by minter.
+     * @param from The address that will be burned the tokens.
+     * @param value The amount of tokens to burn.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function burnByMinter(address from, uint256 value) public onlyMinter returns (bool) {
+        _burn(from, value);
+        return true;
+    }
+}
